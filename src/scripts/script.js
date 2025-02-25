b03ba18bde9dd8d20b579ef12323eef7a45e27ca
richard-piovezan <contato.piovezan@gmail.com>
2025-02-25 20:23:47 -0300

    Create faq section // Create JavaScript for faq questions // create a stylesheet _faq.scss

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const buttons = document.querySelectorAll("[data-tab-button]");
+    const questions = document.querySelectorAll("[data-faq-question]");
     
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", (btn) => {
@@ -7,11 +8,15 @@ document.addEventListener("DOMContentLoaded", () => {
             const tab = document.querySelector(`[data-tab-id=${tabTarget}`);
 
             removeActiveButton();
-            btn.target.classList.add("shows__tabs__button--is-active")
+            btn.target.classList.add("shows__tabs__button--is-active");
 
             hideAllTabs();
             tab.classList.add("shows__list--is-active");
         })
+    }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener("click", openCloseAnswer);
     }
 })
 
@@ -19,7 +24,7 @@ function removeActiveButton() {
     const buttons = document.querySelectorAll("[data-tab-button]")
 
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove("shows__tabs__button--is-active")
+        buttons[i].classList.remove("shows__tabs__button--is-active");
     }
 }
 
@@ -29,4 +34,11 @@ function hideAllTabs() {
     for (i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove("shows__list--is-active");
     }
+}
+
+function openCloseAnswer(element) {
+    const classFaq = "faq__questions__item--is-open";
+    const parentElement = element.target.parentNode;
+
+    parentElement.classList.toggle(classFaq);
 }
